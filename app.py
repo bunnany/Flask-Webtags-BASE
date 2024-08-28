93882daa305aae53f73694e7f592167f0e076990
bunnany <45374304+bunnany@users.noreply.github.com>
2024-08-28 01:17:54 +0000

    final commit of base site

--- app.py
+++ app.py
@@ -1,7 +1,55 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "tags.db"
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def create_connection(db_file):
+    """
+    Creates a connection to the database
+    :parameter    db_file - the name of the file
+    :returns      connection - a connection to the database
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
+
+
+@app.route('/')
+def render_home():
+    return render_template('index.html')
+
+
+@app.route('/webpages')
+def render_webpage():
+    query = "SELECT tag, description FROM html_tags WHERE type='HTML'"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    
+    # Query the DATABASE
+    cur.execute(query)
+    tag_list = cur.fetchall()
+    con.close()
+    print(tag_list)
+    return render_template('webpages.html', tags=tag_list)
+
+
+@app.route('/styles')
+def render_styles():
+    query = "SELECT tag, description FROM html_tags WHERE type='CSS'"
+    con = create_connection(DATABASE)
+    cur = con.cursor()
+    
+    # Query the DATABASE
+    cur.execute(query)
+    tag_list = cur.fetchall()
+    con.close()
+    print(tag_list)
+    return render_template('styles.html', tags=tag_list)
+
+if __name__ == "__main__":
+    app.run()
